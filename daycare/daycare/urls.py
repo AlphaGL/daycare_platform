f8711bf1958e39b083afdda02a77a77433cf907a
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-02-17 08:52:09 +0100

    update

--- daycare/daycare/urls.py
+++ daycare/daycare/urls.py
@@ -6,11 +6,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
-    
+    path('', lambda request: redirect('accounts:login')),
+
     # Core apps
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('accounts.urls')),
